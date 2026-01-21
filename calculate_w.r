fb4ce19378f4fd85bc6ee4b3e9e7930c7cd60d52
sharp-lilac <jessicacboles@gmail.com>
2026-01-21 17:14:01 -0600

    Fetch GBIF occurence data by ID instead of species name

--- calculate_w.r
+++ calculate_w.r
@@ -9,22 +9,21 @@ belize_map <- st_read("shapefiles/Belize_Basemap.shp") %>%
     st_make_valid()
 
 ## Define function to fetch occurrence data for target species from GBIF------------------------
-create_species_sf <- function(species_vector) {
+create_species_sf <- function(id_vector) {
     species_sf <- list()
-    for (species_name in species_vector) {
-        taxon_key <- name_backbone(name = species_name)$usageKey
+    for (id in id_vector) {
         species_data <- occ_search(
-            taxonKey = taxon_key,
+            taxonKey = id,
             hasCoordinate = TRUE
         )
-        sf_name <- paste0("species_sf_", taxon_key)
+        sf_name <- paste0("species_sf_", id)
         species_sf[[sf_name]] <- st_as_sf(species_data$data, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
     }
     return(species_sf)
 }
 
 ## Define function to calculate weights for Belize from occurrence data ------------------------
-calculate_weights_belize <- function(species_vector, species_sf_list) {
+calculate_weights_belize <- function(name_vector, species_sf_list) {
     weights <- c()
     names <- c()
     for (sf_name in names(species_sf_list)) {
@@ -33,17 +32,12 @@ calculate_weights_belize <- function(species_vector, species_sf_list) {
         weight_belize <- sum(num_inside_belize) / nrow(species_sf)
         weights[sf_name] <- weight_belize
     }
-    weights <- data.frame(Source = names(weights), Name = species_vector, Weight = unname(weights))
+    weights <- data.frame(Source = names(weights), Name = name_vector, Weight = unname(weights))
+    weights <- weights %>% filter(Weight > 0.0001)
     return(weights)
 }
 
-## Fetch occurrence data for target species from GBIF ------------------------
-species_names <- c(
-    "Panthera onca", "Leopardus pardalis", "Leopardus wiedii",
-    "Puma concolor", "Puma yagouaroundi"
-)
-species_sf <- create_species_sf(species_names)
-
-## Calculate proportion of occurrences in Belize for target species ------------------------
-weights_belize <- calculate_weights_belize(species_names, species_sf)
+## Calculate proportion of occurrences in Belize for mammals using GBIF occurence data ------------------------
+species_sf <- create_species_sf(belize_redlist_fish$gbif_id)
+weights_belize <- calculate_weights_belize(belize_redlist_fish$species, species_sf)
 weights_belize
