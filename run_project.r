53dda7986676288c6f4b9c3ae3f20e3beff48bbb
Elsy Perez <elsyperez106@gmail.com>
2026-01-23 15:46:13 -0600

    Improve run_project.r instructions

--- run_project.r
+++ run_project.r
@@ -4,8 +4,8 @@
 
 ## Source code ------------------------
 source("load_packages.r") # attach required packages
-source("load_redlist.r") # fetch redlist data from IUCN and create list of Belize species to analyze
-source("calculate_w.r") # calculate weight for species (proportion of ocurrences or range in Belize)
+source("load_redlist.r") # fetch redlist data from IUCN and create list of Belize species to analyze along with their redlist status
+source("calculate_w.r") # calculate weight for species (proportion of occurrences or range in Belize)
 source("calculate_GE.r") # define function to calculate GE for species that have no EDGE score
 source("calculate_EDGE.r") # define function to calculate EDGE for species that have no EDGE score
 source("load_EDGE.r") # load in existing EDGE scores for Belize species
