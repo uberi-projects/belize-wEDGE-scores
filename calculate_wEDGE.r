5dfc06779da5ebe25760d70287eabc2dd84d52fc
sharp-lilac <jessicacboles@gmail.com>
2026-01-26 15:57:35 -0600

    Refine wEDGE csv output

--- calculate_wEDGE.r
+++ calculate_wEDGE.r
@@ -9,49 +9,57 @@ df_EDGE_all_combined <- df_EDGE_all %>%
 wEDGE_birds <- weights_belize_birds %>%
     left_join(df_EDGE_all_combined, by = "species") %>%
     filter(!is.na(EDGE)) %>%
-    mutate(wEDGE = EDGE * weight, Type = "Bird", gbif_id = as.numeric(gbif_id))
+    mutate(wEDGE = EDGE * weight, Type = "Bird", gbif_id = as.numeric(gbif_id)) %>%
+    left_join(select(belize_redlist_birds, red_list_category_code, original_species), by = join_by(species == original_species))
 
 ## Assemble wEDGE for amphibians ------------------------
 wEDGE_amphibians <- weights_belize_amphibians %>%
     left_join(df_EDGE_all_combined, by = "species") %>%
     filter(!is.na(EDGE)) %>%
-    mutate(wEDGE = EDGE * weight, Type = "Amphibian", gbif_id = as.numeric(gbif_id))
+    mutate(wEDGE = EDGE * weight, Type = "Amphibian", gbif_id = as.numeric(gbif_id)) %>%
+    left_join(select(belize_redlist_amphibians, red_list_category_code, original_species), by = join_by(species == original_species))
 
 ## Assemble wEDGE for mammals ------------------------
 wEDGE_mammals <- weights_belize_mammals %>%
     left_join(df_EDGE_all_combined, by = "species") %>%
     filter(!is.na(EDGE)) %>%
-    mutate(wEDGE = EDGE * weight, Type = "Mammal", gbif_id = as.numeric(gbif_id))
+    mutate(wEDGE = EDGE * weight, Type = "Mammal", gbif_id = as.numeric(gbif_id)) %>%
+    left_join(select(belize_redlist_mammals, red_list_category_code, original_species), by = join_by(species == original_species))
 
 ## Assemble wEDGE for reptiles ------------------------
 wEDGE_reptiles <- weights_belize_reptiles %>%
     left_join(df_EDGE_all_combined, by = "species") %>%
     filter(!is.na(EDGE)) %>%
-    mutate(wEDGE = EDGE * weight, Type = "Reptile", gbif_id = as.numeric(gbif_id))
+    mutate(wEDGE = EDGE * weight, Type = "Reptile", gbif_id = as.numeric(gbif_id)) %>%
+    left_join(select(belize_redlist_reptiles, red_list_category_code, original_species), by = join_by(species == original_species))
 
 ## Assemble wEDGE for turtles ------------------------
 wEDGE_turtles <- weights_belize_turtles %>%
     left_join(df_EDGE_all_combined, by = "species") %>%
     filter(!is.na(EDGE)) %>%
-    mutate(wEDGE = EDGE * weight, Type = "Turtle", gbif_id = as.numeric(gbif_id))
+    mutate(wEDGE = EDGE * weight, Type = "Turtle", gbif_id = as.numeric(gbif_id)) %>%
+    left_join(select(belize_redlist_turtles, red_list_category_code, original_species), by = join_by(species == original_species))
 
 ## Assemble wEDGE for freshwater fish ------------------------
 wEDGE_fish_freshwater <- weights_belize_fish_freshwater %>%
     left_join(df_EDGE_all_combined, by = "species") %>%
     filter(!is.na(EDGE)) %>%
-    mutate(wEDGE = EDGE * weight, Type = "Freshwater Fish", gbif_id = as.numeric(gbif_id))
+    mutate(wEDGE = EDGE * weight, Type = "Freshwater Fish", gbif_id = as.numeric(gbif_id)) %>%
+    left_join(select(belize_redlist_fish_freshwater, red_list_category_code, taxon_scientific_name), by = join_by(species == taxon_scientific_name))
 
 ## Assemble wEDGE for marine fish ------------------------
 wEDGE_fish_marine <- weights_belize_fish_marine %>%
     left_join(df_EDGE_all_combined, by = "species") %>%
     filter(!is.na(EDGE)) %>%
-    mutate(wEDGE = EDGE * weight, Type = "Marine Fish", gbif_id = as.numeric(gbif_id))
+    mutate(wEDGE = EDGE * weight, Type = "Marine Fish", gbif_id = as.numeric(gbif_id)) %>%
+    left_join(select(belize_redlist_fish_marine, red_list_category_code, taxon_scientific_name), by = join_by(species == taxon_scientific_name))
 
 ## Assemble wEDGE for mixed habitat fish ------------------------
 wEDGE_fish_mixed <- weights_belize_fish_mixed %>%
     left_join(df_EDGE_all_combined, by = "species") %>%
     filter(!is.na(EDGE)) %>%
-    mutate(wEDGE = EDGE * weight, Type = "Mixed Fish", gbif_id = as.numeric(gbif_id))
+    mutate(wEDGE = EDGE * weight, Type = "Mixed Fish", gbif_id = as.numeric(gbif_id)) %>%
+    left_join(select(belize_redlist_fish_mixed, red_list_category_code, taxon_scientific_name), by = join_by(species == taxon_scientific_name))
 
 ## Create directory for output wEDGE scores ------------------------
 directory_wEDGE <- "outputs/wEDGE"
@@ -64,5 +72,6 @@ wEDGE_all <- bind_rows(
     wEDGE_birds, wEDGE_amphibians, wEDGE_mammals, wEDGE_reptiles, wEDGE_turtles,
     wEDGE_fish_freshwater, wEDGE_fish_marine, wEDGE_fish_mixed
 ) %>%
+    select(Type, Species = species, Common_Names = Common.names, w = weight, EDGE, wEDGE, Redlist = red_list_category_code) %>%
     arrange(Type, -wEDGE)
 write.csv(wEDGE_all, file.path(directory_wEDGE, "wEDGE_all.csv"))
